1162ffb55bef6d02580fd546f714f960e266d80c
ohick <oliviahick1@gmail.com>
2020-02-05 10:17:43 +0100

    webpack install

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const webpack = require('webpack');
+
+console.log("WAT");
+
+module.exports = {
+    mode: 'development',
+    //entry: path.resolve(__dirname, 'src/client'),
+    entry: './src/client'
+};
