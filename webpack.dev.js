d40b8fa1aaf5f942b238c128622619e65e6eed55
ohick <oliviahick1@gmail.com>
2020-02-06 16:39:41 +0100

    last commit

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   mode: 'development',
@@ -21,5 +23,12 @@ module.exports = {
       template: './src/client/views/index.html',
       filename: './index.html',
     }),
+    new CleanWebpackPlugin({
+      dry: true,
+      verbose: true,
+      cleanSlateWebpackAssets: true,
+      protectWebpackAssets: false,
+    }),
+    new BundleAnalyzerPlugin(),
   ],
 };
